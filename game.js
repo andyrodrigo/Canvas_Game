46e6e46065d01955f1af3cb201adeebc551e0556
perkles <andyrodrigo2@gmail.com>
2022-08-08 15:32:16 -0300

    colisao

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var personagem;
+var obstaculo;
 
 function startGame() {
     areaDoJogo.start();
     personagem = new componente(30, 30, "red", 10, 120);
+    obstaculo = new componente(10, 200, "green", 300, 120);
 }
 
 var areaDoJogo = {
@@ -16,18 +18,21 @@ var areaDoJogo = {
 
         this.interval = setInterval( atualizaAreaDoJogo, 20 );
 
-        window.addEventListener('keydown', function (e) {
-            //areaDoJogo.key = e.keyCode;//Deprecated
-            areaDoJogo.key = e.code;
+        window.addEventListener('keydown', function (e) { //Ao pressionar tecla
+            areaDoJogo.keys = (areaDoJogo.keys || []);
+            areaDoJogo.keys[e.code] = true;
         })
-        window.addEventListener('keyup', function (e) {
-            areaDoJogo.key = false;
+        window.addEventListener('keyup', function (e) { //Ao soltar tecla
+            areaDoJogo.keys[e.code] = false;
         })
 
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+    },
+    stop : function() {
+        clearInterval(this.interval);
+      }
 
 }
 
@@ -49,27 +54,48 @@ function componente( width, height, color, x, y) {
         this.x += this.velocidadeX;
         this.y += this.velocidadeY;
     }
+
+    this.colisaoCom = function( outroObjeto) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = outroObjeto.x;
+        var otherright = outroObjeto.x + (outroObjeto.width);
+        var othertop = outroObjeto.y;
+        var otherbottom = outroObjeto.y + (outroObjeto.height);
+        var colisao = true;
+        if ((mybottom < othertop) ||
+            (mytop > otherbottom) ||
+            (myright < otherleft) ||
+            (myleft > otherright)) {
+            colisao = false;
+        }
+        return colisao;
+      }
 }
 
 function atualizaAreaDoJogo() {
-    areaDoJogo.clear();
-    personagem.velocidadeX = 0;
-    personagem.velocidadeY = 0;
 
-    if (areaDoJogo.key && areaDoJogo.key == 'ArrowLeft') {personagem.velocidadeX = -1; }
-    if (areaDoJogo.key && areaDoJogo.key == 'ArrowRight') {personagem.velocidadeX = 1; }
-    if (areaDoJogo.key && areaDoJogo.key == 'ArrowUp') {personagem.velocidadeY = -1; }
-    if (areaDoJogo.key && areaDoJogo.key == 'ArrowDown') {personagem.velocidadeY = 1;}
-    /* //Deprecated
-    if (areaDoJogo.key && areaDoJogo.key == 37) {personagem.velocidadeX = -1; }
-    if (areaDoJogo.key && areaDoJogo.key == 39) {personagem.velocidadeX = 1; }
-    if (areaDoJogo.key && areaDoJogo.key == 38) {personagem.velocidadeY = -1; }
-    if (areaDoJogo.key && areaDoJogo.key == 40) {personagem.velocidadeY = 1; }*/
+    if ( personagem.colisaoCom( obstaculo ) ) {
+        areaDoJogo.stop();
+    } else {
+        areaDoJogo.clear();
+        personagem.velocidadeX = 0;
+        personagem.velocidadeY = 0;
+    
+        if (areaDoJogo.keys && areaDoJogo.keys['ArrowLeft']  ) {personagem.velocidadeX = -1; }
+        if (areaDoJogo.keys && areaDoJogo.keys['ArrowRight'] ) {personagem.velocidadeX = 1; }
+        if (areaDoJogo.keys && areaDoJogo.keys['ArrowUp']    ) {personagem.velocidadeY = -1; }
+        if (areaDoJogo.keys && areaDoJogo.keys['ArrowDown']  ) {personagem.velocidadeY = 1;}
+    
+        obstaculo.update();
+        personagem.newPos();
+        personagem.update();
+    }
 
-    personagem.newPos();
-    personagem.update();
 }
-
+/*
 function moveAcima() {
     personagem.velocidadeY -= 1;
 }
@@ -89,4 +115,4 @@ function moveDireita() {
 function paraMovimento() {
     personagem.velocidadeX = 0;
     personagem.velocidadeY = 0;
-}
+}*/
